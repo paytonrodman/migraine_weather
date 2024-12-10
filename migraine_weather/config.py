dabb654c603f7a5efd6d1db290c7909ac348945f
Payton Rodman <rodman.payton@gmail.com>
2024-12-10 16:16:40 +0000

    Removed unused external data folder reference

--- migraine_weather/config.py
+++ migraine_weather/config.py
@@ -14,7 +14,6 @@ DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
-EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
 MODELS_DIR = PROJ_ROOT / "models"
 
