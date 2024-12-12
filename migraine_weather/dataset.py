89490129f28a562479d013012af2887ecb40b88c
Payton Rodman <rodman.payton@gmail.com>
2024-12-12 13:32:55 +0000

    Updated to reflect updated make_dataset modules

--- migraine_weather/dataset.py
+++ migraine_weather/dataset.py
@@ -8,7 +8,6 @@ import glob
 
 from migraine_weather.config import RAW_DATA_DIR, PROCESSED_DATA_DIR
 from migraine_weather.data import make_dataset
-from migraine_weather.custom_funcs import get_country_codes
 from datetime import datetime
 
 app = typer.Typer()
@@ -22,21 +21,20 @@ def main(
     # ----------------------------------------------
 ):
 
-    country_codes = get_country_codes()
+    country_codes = make_dataset.get_country_codes()
     data_files = [d.split('/')[-1][:2] for d in glob.glob(str(output_path / '*'))]
-
-    country_codes = [c for c in country_codes if c[0] in ['E', 'F', 'G']]
 
     start = datetime(2010, 1, 1, 0, 0, 0)
     end = datetime(2020, 12, 31, 23, 59, 59)
 
     logger.info("Checking if processed data exists...")
     for cc in country_codes:
-        file_exists = make_dataset.check_file_exists(cc, data_files, output_path, overwrite_flag=1)
+        file_exists = make_dataset.check_file_exists(cc, data_files, overwrite_flag=1)
 
         if not file_exists:
             logger.info(f"Generating dataset for {cc}...")
-            make_dataset.make_dataset(cc, start, end, output_path)
+            data = make_dataset.make_dataset(cc, start, end)
+            data.to_csv(output_path / f"{cc}.csv")
 
     logger.success("Processing dataset complete.")
     # -----------------------------------------
