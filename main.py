b1e1e30b64250a83234d29b1c8baae3602825020
Payton Rodman <rodman.payton@gmail.com>
2025-12-30 19:01:09 +0100

    and also formatting main

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ from migraine_weather import processing
 def main(
     input_path: Path = Path(RAW_DATA_DIR.format(DATA_DIR)),
     interim_output_path: Path = Path(INTERIM_DATA_DIR.format(DATA_DIR)),
-    processed_output_path: Path = Path(PROCESSED_DATA_DIR.format(DATA_DIR))
+    processed_output_path: Path = Path(PROCESSED_DATA_DIR.format(DATA_DIR)),
 ):
     # get list of country codes from existing datafiles
-    data_files = [d.split('/')[-1][:2] for d in glob.glob(str(interim_output_path / '*'))]
+    data_files = [d.split("/")[-1][:2] for d in glob.glob(str(interim_output_path / "*"))]
 
     # date range to analyse
     start = datetime(2010, 1, 1, 0, 0, 0)
     end = datetime(2020, 12, 31, 23, 59, 59)
 
     # get dataframe containing all stations with required Hourly data around the world
-    eligible_stations = processing.get_eligible_stations('hourly', start, end)
+    eligible_stations = processing.get_eligible_stations("hourly", start, end)
 
     # loop through countries, checking if file exists/should be overwritten
     # then make the dataset for that country
-    for country_code, country_stations in eligible_stations.groupby(by='country'):
+    for country_code, country_stations in eligible_stations.groupby(by="country"):
         country_code = str(country_code)
         file_exists = processing.check_file_exists(country_code, data_files, overwrite=False)
 
